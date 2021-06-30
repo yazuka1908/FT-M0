a2cac677e617572be11fc88672d32a0b1f74de0e
JamesTellez <jamestellez@subocol.com>
2021-06-29 21:08:08 -0500

    Primer tarea

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -12,7 +12,7 @@ function mayoriaDeEdad(edad) {
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
 }
-  
+
 function conection(status) {
   //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
   //Cuando el estado es igual a 1, el usuario está "Online"
@@ -49,12 +49,43 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+  var respuestaColor = "";
+
+  switch (color) {
+    case "blue":
+      respuesta = "this is blue";
+      break;
+    case "red":
+      respuesta = "this is red";
+      break;
+    case "green":
+      respuesta = "this is green";
+      break;
+    case "orange":
+      respuesta = "this is orange";
+      break;
+    default:
+      respuesta = "Color not found"
+      break;
+  }
+
+  return respuesta;
 }
 
 function esDiezOCinco(numero) {
   // Devuelve "true" si "numero" es 10 o 5
   // De lo contrario, devuelve "false"
   // Tu código:
+  if (((numero > 1) && (numero <10)) || (numero === 5)){
+    return true;
+  }
+
+  if (numero === 10){
+    return true;
+  } else if (numero === 5){
+    return true;
+  }
+
 }
 
 function estaEnRango(numero) {
@@ -82,11 +113,35 @@ function fizzBuzz(numero) {
 
 function operadoresLogicos(num1, num2, num3) {
   //La función recibe tres números distintos. 
-  //Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
-  //Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
-  //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
-  //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
-  //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  //1. Si num1 es mayor a num2 y a num3 y además es positivo, retornar ---> "Número 1 es mayor y positivo"
+  //2. Si alguno de los tres números es negativo, retornar ---> "Hay negativos"
+  //3. Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
+  //4. 0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
+  //5. Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+ 
+  // punto 2
+  if ((num1 < 0) || (num2 < 0) || (num3 < 0)){
+    return "Hay negativos";
+  }
+  
+  // punto 4
+  if ((num1 === 0) || (num2 ===0) || (num3 === 0)){
+    return "Error";
+  }
+
+  // punto 1
+  if ((num1 > num2) && (num1 > num3) && (num1 > 0)){
+    return "Número 1 es mayor y positivo";
+  }
+
+  // punto 3
+  if ((num3 > num2) && (num3 > num2)){
+    var suma = num3 + 1;
+    return suma;
+  }
+
+  // punto 5
+  return false;
 }
 
 function esPrimo(numero) {
@@ -97,17 +152,17 @@ function esPrimo(numero) {
   // Nota: Los números 0 y 1 NO son considerados números primos
 }
 
-function esVerdadero(valor){
+function esVerdadero(valor) {
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
 
 }
 
-function tieneTresDigitos(numero){
+function tieneTresDigitos(numero) {
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
-  
+
 }
 
 function doWhile(numero) {
